4ff5a0c13263b5f73d078efcd5c8539ba58e9f3e
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-12-09 19:18:38 +0100

    [FEATURE][HOME] * Custom Cards was added

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Text('Hola mundo'),
-      )
+        appBar: AppBar(
+          centerTitle: false,
+          title: Text('En Cartelera'),
+          backgroundColor: Colors.indigoAccent,
+          actions: [IconButton(icon: Icon(Icons.search), onPressed: () {})],
+        ),
+        body: Container(
+          child: SafeArea(
+            child: Column(
+              children: [
+                _cardProvider(),
+              ],
+            ),
+          ),
+        ));
+  }
+
+  Widget _cardProvider() {
+    Swiper _swiper = new Swiper(
+      itemBuilder: (BuildContext context, int index) {
+        return new Image.network(
+          "http://via.placeholder.com/350x150",
+          fit: BoxFit.fill,
+        );
+      },
+      itemCount: 3,
+      // pagination: new SwiperPagination(),
+      // control: new SwiperControl(),
+      layout: SwiperLayout.STACK,
+      itemWidth: 300,
     );
+
+    return Container(padding: EdgeInsets.only(top: 20),width: double.infinity, height: 250, child: _swiper);
   }
 }

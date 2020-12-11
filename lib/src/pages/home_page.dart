23d48aba3f28130d5eba9165f3532703d57dc879
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-12-11 07:25:55 +0100

    [FEATURE][PROVIDER] * New endpoint was implemented and code was refactor.

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -3,6 +3,9 @@ import 'package:films/src/widgets/card_swiper_widget_provider.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+
+  final provider = new FilmsProvider();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,29 +18,50 @@ class HomePage extends StatelessWidget {
         body: Container(
           child: SafeArea(
             child: Column(
-              children: [
-                _cardWidgetProvider(),
-              ],
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [_cardWidgetProvider(provider), _footer(context, provider)],
             ),
           ),
         ));
   }
 
-  Widget _cardWidgetProvider() {
-    final provider = new FilmsProvider();
+  Widget _cardWidgetProvider(FilmsProvider provider) {
 
     return FutureBuilder(
       future: provider.getNowPlaying(),
-      builder: (BuildContext constext, AsyncSnapshot<List> snapshot) {
+      builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
         if (snapshot.hasData) {
           return CardSwiperWidgetProvider(films: snapshot.data);
         } else {
           return Container(
-            height: 400,
-              child: Center(child: CircularProgressIndicator())
-          );
+              height: 400, child: Center(child: CircularProgressIndicator()));
         }
       },
     );
+  }
+
+  Widget _footer(BuildContext context, FilmsProvider provider) {
+    return Container(
+        width: double.infinity,
+        child: Column(
+          children: [
+            // Text('Populares', style: Theme.of(context).textTheme.subtitle1,),
+            Text(
+              'Populares',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            FutureBuilder(
+              future: provider.getPopular(),
+              builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+                if (snapshot.hasData) {
+                  snapshot.data.forEach((film) => {
+                    film.title
+                  });
+                }
+                return Container();
+              },
+            )
+          ],
+        ));
   }
 }
--- lib/src/providers/films_provider.dart
+++ lib/src/providers/films_provider.dart
@@ -1,24 +1,30 @@
 import 'dart:convert';
-import 'package:http/http.dart' as http;
+
 import 'package:films/src/models/film_model.dart';
+import 'package:http/http.dart' as http;
 
 class FilmsProvider {
   String _apiKey = 'eb3289f4bb07a4659a97025942a3c2b7';
   String _url = 'api.themoviedb.org';
   String _language = 'es-ES';
 
-  Future<List<Film>> getNowPlaying() async {
-    
-    final uri = Uri.https(_url, '3/movie/now_playing', {
-      'api_key': _apiKey,
-      'language': _language
-    });
-
+  Future<List<Film>> _responseProcess(Uri uri) async {
     final response = await http.get(uri);
-
     final decodedData = json.decode(response.body);
-
     final films = new Films.fromJsonList(decodedData['results']);
     return films.films;
+  }
+
+  Future<List<Film>> getNowPlaying() async {
+    final uri = Uri.https(_url, '3/movie/now_playing',
+        {'api_key': _apiKey, 'language': _language});
+
+    return await _responseProcess(uri);
+  }
+
+  Future<List<Film>> getPopular() async {
+    final uri = Uri.https(
+        _url, '3/movie/popular', {'api_key': _apiKey, 'language': _language});
+    return await _responseProcess(uri);
   }
 }

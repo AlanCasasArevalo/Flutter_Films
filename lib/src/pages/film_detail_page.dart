9a2f987dbc93062101bbafff4e31bd61bd9cbda2
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-12-13 09:44:32 +0100

    [FEATURE][FILM DETAIL] * Film detail Description, image and header was added

--- lib/src/pages/film_detail_page.dart
+++ lib/src/pages/film_detail_page.dart
@@ -2,10 +2,8 @@ import 'package:films/src/models/film_model.dart';
 import 'package:flutter/material.dart';
 
 class FilmDetailPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-
     final Film film = ModalRoute.of(context).settings.arguments;
 
     return Scaffold(
@@ -13,13 +11,71 @@ class FilmDetailPage extends StatelessWidget {
         child: CustomScrollView(
           slivers: [
             _appBarBuilder(film),
+            SliverList(
+                delegate: SliverChildListDelegate([
+              SizedBox(height: 10),
+              _titlePoster(context, film),
+              _descriptionFilm(film),
+            ]))
           ],
         ),
       ),
     );
   }
 
-  SliverAppBar _appBarBuilder (Film film) {
+  Widget _descriptionFilm(Film film) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+      child: Text(
+        film.overview,
+        textAlign: TextAlign.justify,
+      ),
+    );
+  }
+
+  Widget _titlePoster(BuildContext context, Film film) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image(
+              image: NetworkImage(film.getPosterImage()),
+              height: 150,
+            ),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          Flexible(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(film.title,
+                  style: Theme.of(context).textTheme.headline6,
+                  overflow: TextOverflow.ellipsis),
+              Text(
+                film.originalTitle,
+                style: Theme.of(context).textTheme.subtitle1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              Row(
+                children: [
+                  Icon(Icons.star_border),
+                  Text(film.voteAverage.toString(),
+                      style: Theme.of(context).textTheme.subtitle1,
+                      overflow: TextOverflow.ellipsis)
+                ],
+              )
+            ],
+          ))
+        ],
+      ),
+    );
+  }
+
+  SliverAppBar _appBarBuilder(Film film) {
     return SliverAppBar(
       elevation: 30,
       backgroundColor: Colors.indigoAccent,
@@ -28,7 +84,10 @@ class FilmDetailPage extends StatelessWidget {
       pinned: true,
       flexibleSpace: FlexibleSpaceBar(
         centerTitle: true,
-        title: Text(film.title, style: TextStyle(color: Colors.white, fontSize: 16),),
+        title: Text(
+          film.title,
+          style: TextStyle(color: Colors.white, fontSize: 16),
+        ),
         background: FadeInImage(
           image: NetworkImage(film.getBackdropPathImage()),
           placeholder: AssetImage('assets/loading.gif'),
@@ -38,4 +97,6 @@ class FilmDetailPage extends StatelessWidget {
       ),
     );
   }
+
+
 }

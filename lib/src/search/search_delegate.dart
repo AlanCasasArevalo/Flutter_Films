2f1fd2f8c93eced918f4cacadf162e37722d0996
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-12-14 19:49:15 +0100

    [FEATURE][SEARCH] * Search bar was improved

--- lib/src/search/search_delegate.dart
+++ lib/src/search/search_delegate.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
 
 class DataSearch extends SearchDelegate {
+
+  String selected = '';
+
+  final films = [
+    'Spiderman',
+    'Capitan america',
+    'Ironman',
+    'Superman',
+    'Shazam',
+    'Jiu Jitsu',
+    'Algo normal'
+  ];
+
+  final recentFilms = ['Spiderman', 'Capitan america', 'Ironman'];
+
   @override
   List<Widget> buildActions(BuildContext context) {
     // Acciones del appbar
@@ -30,12 +45,37 @@ class DataSearch extends SearchDelegate {
   @override
   Widget buildResults(BuildContext context) {
     // Crea los resultados a mostrar
-    return Container();
+    return Center(
+        child: Container(
+          height: 100,
+          width: 100,
+          color: Colors.amberAccent,
+          child: Text(selected),
+        )
+    );
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
     // Crea las sugerencias cuando la persona escribe
-    return Container();
+
+    final suggestionList = (query.isEmpty) ? recentFilms : films.where(
+            // Se puede usar el contiene o bien el startsWith
+            // (element) => element.toLowerCase().startsWith(query.toLowerCase())
+            (element) => element.toLowerCase().contains(query.toLowerCase())
+    ).toList();
+
+    return ListView.builder(
+        itemCount: suggestionList.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: Icon(Icons.movie),
+            title: Text(suggestionList[index]),
+            onTap: () {
+              selected = suggestionList[index];
+              showResults(context);
+            },
+          );
+        });
   }
 }
